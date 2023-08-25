5b01badb7dfbea2d8c933386bfd4d22a7c97185d
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2023-08-25 13:27:12 +0300

    Setup base project structure

--- src/main/java/com/example/citiesapp/controller/StartupController.java
+++ src/main/java/com/example/citiesapp/controller/StartupController.java
@@ -1,9 +1,9 @@
-package com.example.citiesapp;
+package com.example.citiesapp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StartupController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module com.example.citiesapp {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
 
     opens com.example.citiesapp to javafx.fxml;
     exports com.example.citiesapp;
+    exports com.example.citiesapp.controller;
+    opens com.example.citiesapp.controller to javafx.fxml;
 }
