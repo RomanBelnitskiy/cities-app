db666fc3d0e536e84eb3a43a15a97dc3b680ab45
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2023-08-29 06:59:53 +0300

    Packages renamed

--- src/main/java/com/example/citiesapp/AppLauncher.java
+++ src/main/java/com/example/citiesapp/AppLauncher.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CitiesApplication extends Application {
+public class AppLauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CitiesApplication.class.getResource("/view/startup-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppLauncher.class.getResource("/views/startup-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
